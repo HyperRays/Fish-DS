c19c62474d6b69238d1c9b166e5accf181498aa7
HyperRays <soham.kuvalekar@stud.edubs.ch>
2023-10-17 09:31:11 +0200

    better readability and usage of makefile

--- docker_install_scripts/install-docker.sh
+++ docker_install_scripts/install-docker.sh
@@ -1,28 +1,28 @@
-sudo apt update
+    sudo apt update
 
-sudo apt install -y apt-transport-https ca-certificates software-properties-common
+    sudo apt install -y apt-transport-https ca-certificates software-properties-common
 
-# add docker to known linux keyring
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > docker-repo-keyring.gpg
-sudo mv ./docker-repo-keyring.gpg /etc/apt/trusted.gpg.d/
-sudo chown root:root /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
-sudo chmod ugo+r /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
-sudo chmod go-w /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-
-
-# install docker packages
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Install newuidmap & newgidmap binaries
-apt install -y uidmap
+    # add docker to known linux keyring
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > docker-repo-keyring.gpg
+    sudo mv ./docker-repo-keyring.gpg /etc/apt/trusted.gpg.d/
+    sudo chown root:root /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
+    sudo chmod ugo+r /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
+    sudo chmod go-w /etc/apt/trusted.gpg.d/docker-repo-keyring.gpg
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
 
 
+    # install docker packages
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# rootless docker additional installation script
-# sudo apt-get install -y docker-ce-rootless-extras
-# dockerd-rootless-setuptool.sh install
+    # Install newuidmap & newgidmap binaries
+    apt install -y uidmap
 
 
-sh start_docker.sh
+
+    # rootless docker additional installation script
+    # sudo apt-get install -y docker-ce-rootless-extras
+    # dockerd-rootless-setuptool.sh install
+
+
+    sh start_docker.sh
---
+++ fish-basic/matlab/replay.m
@@ -0,0 +1,3 @@
+for i = 1:4
+drawslice('data',i,2,1e6*[-1 1 -1 1 -1 1]);
+end
