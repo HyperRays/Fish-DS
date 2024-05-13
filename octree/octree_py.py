18ddcaa77b5ccd327257dbdaa188a70a31800d39
servrcube <63092977+servrcube@users.noreply.github.com>
2024-05-13 22:27:16 +0200

    functioning abs pos

--- octree/octree_py.py
+++ octree/octree_py.py
@@ -49,6 +49,10 @@ class Vec3:
                 self.y & other.y,
                 self.z & other.z)
 
+    def __iter__(self):
+        for v in (self.x,self.y,self.z):
+            yield v
+
 @dataclass
 class Node:
     children: list
@@ -78,11 +82,12 @@ map_child = {
     Vec3(1,1,1): 8
 }
 
+#                           |Debug only          |            
 def create_full_tree(depth, position = Vec3(0,0,0), counter = 1):
     if counter == depth:
         return Node([None for _ in range(8)],(position, counter))
     
-    return Node([create_full_tree(depth, position + map_pos[i+1] << (counter-1), counter+1) for i in range(8)],(position, counter))
+    return Node([create_full_tree(depth, position + (map_pos[i+1] << (counter-1)), counter+1) for i in range(8)],(position, counter))
 
 ## naive code
 # def bfs_find_node(root: Node, target: Vec3, depth: int):
@@ -94,29 +99,51 @@ def create_full_tree(depth, position = Vec3(0,0,0), counter = 1):
 #         if value != None:
 #             return value
 
+
 # use postion as map
-def find_position(root: Node, target: Vec3, depth: int):
+def find_position(root: Node, target: Vec3, depth: int, last_found = False):
     for i in range(depth-1):
+        
         idx = map_child[(target >> i) & Vec3(1,1,1)]
         node = root.children[idx-1]
-        if node == None:
+
+        if node is None and not last_found:
             return None
-        else:
-            root = node
+        if last_found:
+            return root
+        root = node
         
     return root
 
 def addr_to_abs(position: Vec3, max_depth):
-    return sum((((position >> i) & Vec3(1,1,1))*(1/2**(i+1)) for i in range(max_depth-1)), start=Vec3(0,0,0))
+    position = position << 1
+    return Vec3(1/position.x,1/position.y,1/position.z)
+    # return sum((((position >> i) & Vec3(1,1,1))*(1/2**(i+1)) for i in range(max_depth-1)), start=Vec3(0,0,0))
+
+def abs_to_addr(position: Vec3, max_depth):
+    return to_int(Vec3(1/position.x,1/position.y,1/position.z))
+
+def to_int(vec):
+    return Vec3(
+        int(vec.x),
+        int(vec.y),
+        int(vec.z)
+    )
+        
 
 
-root = create_full_tree(2)
-pprint.pp(root)
+root = create_full_tree(4)
+# pprint.pp(root)
 
-print(addr_to_abs(Vec3(6,2,4), 5))
-
-search = find_position(root, Vec3(2,2,2), 3)
+search = find_position(root, Vec3(2,4,3), 4)
 if search != None:
     print(search.data)
 else:
     print("Found Nothing")
+
+v= search.data[0]
+abs_addr = addr_to_abs(v, 6)
+rel_addr = to_int(abs_to_addr(abs_addr, 6))
+
+print(abs_addr)
+print(rel_addr >> 1)
