ff13efa2c6e7afb917a3a87ba334cb10006c9270
HyperRays <soham.kuvalekar@stud.edubs.ch>
2024-05-15 19:34:14 +0200

    octree add impl

--- octree/octree_py.py
+++ octree/octree_py.py
@@ -3,6 +3,7 @@
 # 1 indexed to ease reimplementation in fortran
 from dataclasses import dataclass
 import pprint
+from typing import Any
 
 @dataclass(slots=True, frozen=True)
 class Vec3:
@@ -56,7 +57,7 @@ class Vec3:
 @dataclass
 class Node:
     children: list
-    data: any
+    data: Any
     
 map_pos = {
     1: Vec3(0,0,0),
@@ -115,35 +116,41 @@ def find_position(root: Node, target: Vec3, depth: int, last_found = False):
         
     return root
 
-def addr_to_abs(position: Vec3, max_depth):
+def inv(v):
+    if v == 0:
+        return 0 
+    return 1/v
+
+def addr_to_abs(position: Vec3):
     position = position << 1
-    return Vec3(1/position.x,1/position.y,1/position.z)
-    # return sum((((position >> i) & Vec3(1,1,1))*(1/2**(i+1)) for i in range(max_depth-1)), start=Vec3(0,0,0))
+    return Vec3(inv(position.x),inv(position.y),inv(position.z))
 
-def abs_to_addr(position: Vec3, max_depth):
-    return to_int(Vec3(1/position.x,1/position.y,1/position.z))
-
+def abs_to_addr(position: Vec3):
+    return to_int(Vec3(inv(position.x),inv(position.y),inv(position.z))) >> 1
+ 
 def to_int(vec):
     return Vec3(
         int(vec.x),
         int(vec.y),
         int(vec.z)
     )
-        
 
 
-root = create_full_tree(4)
+max_height = 2
+
+root = create_full_tree(max_height)
 # pprint.pp(root)
 
-search = find_position(root, Vec3(2,4,3), 4)
+search = find_position(root, Vec3(0,0,0), 2)
 if search != None:
-    print(search.data)
+    print("Search Result:",search.data)
 else:
     print("Found Nothing")
 
-v= search.data[0]
-abs_addr = addr_to_abs(v, 6)
-rel_addr = to_int(abs_to_addr(abs_addr, 6))
+v = search.data[0] # type: ignore
+abs_addr = addr_to_abs(v) + Vec3(0,0,1/2**(2-1))
+rel_addr = to_int(abs_to_addr(abs_addr))
 
-print(abs_addr)
-print(rel_addr >> 1)
+print("Absolute Address:",abs_addr)
+print("Relative Address", rel_addr)
+print("New Node",find_position(root, rel_addr, max_height).data) # type: ignore
